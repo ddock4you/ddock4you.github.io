e6cb5b1951052450d052ee753c876d5a95642ae6
ddock4you <ddock4you@gmail.com>
2020-05-05 23:09:35 +0900

    - 1차 포트폴리오 배포

---
+++ precache-manifest.45344bcbc17ac5b8f7f1c5a7fe140bea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bad430b59b50dfcc8e58c7642994ab6",
+    "url": "/youtube-music-player/index.html"
+  },
+  {
+    "revision": "bf37898d264a655f9e46",
+    "url": "/youtube-music-player/static/css/2.6d7d54d2.chunk.css"
+  },
+  {
+    "revision": "45ddb705c48db65348fc",
+    "url": "/youtube-music-player/static/css/main.c07850b9.chunk.css"
+  },
+  {
+    "revision": "bf37898d264a655f9e46",
+    "url": "/youtube-music-player/static/js/2.948dcd2a.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/youtube-music-player/static/js/2.948dcd2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45ddb705c48db65348fc",
+    "url": "/youtube-music-player/static/js/main.7e7c6657.chunk.js"
+  },
+  {
+    "revision": "0bc584fa320458196c81",
+    "url": "/youtube-music-player/static/js/runtime-main.3de20c12.js"
+  }
+]);
